176143b2ad6362aeff2939c6b910b1731789bb43
Steve Warren <steve@interactivenyc.com>
2018-05-16 20:04:53 -0400

    adding log statements to 001-sign-up

--- 001-sign-up/src/App.js
+++ 001-sign-up/src/App.js
@@ -4,15 +4,17 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 var Input = React.createClass({
 	render: function() {
+		console.log("[Input] render id: ", this.props.id);
+
 		return (
 			<div className="Input">
-				<input 
+				<input
 					id={this.props.id}
 					autoComplete="false"
 					required
 					type={this.props.type}
 					placeholder={this.props.placeholder}
-				/>	
+				/>
 				<label htmlFor={this.props.id}></label>
 			</div>
 		);
@@ -21,14 +23,17 @@ var Input = React.createClass({
 
 var Modal = React.createClass({
 	render: function() {
+		console.log("[Modal] render");
+
 		return (
 			<div className="Modal">
-				<form 
+				<form
 					onSubmit={this.props.onSubmit}
 					className="ModalForm">
 					<Input
 						id="name"
 						type="text"
+						ref="nameInput"
 						placeholder="Jack-Edward Oliver" />
 					<Input
 						id="username"
@@ -48,30 +53,43 @@ var Modal = React.createClass({
 });
 
 var App = React.createClass({
-	
+
+
 	getInitialState: function() {
+		console.log('[App] getInitialState mounted = false');
 		return { mounted: false };
 	},
-	
+
 	componentDidMount: function() {
+		console.log('[App] componentDidMount mounted = true');
+
 		this.setState({ mounted: true });
 	},
-	
+
 	handleSubmit: function(e) {
+		console.log("[App handleSubmit]");
+
 		this.setState({ mounted: false });
 		e.preventDefault();
 	},
 
 	render: function() {
+		console.log("[App] render mounted: ", this.state.mounted);
+
 		var child;
 
 		if(this.state.mounted) {
+			console.log('[App] render - this.state.mounted = true');
+			console.log('[App] render - child = Modal');
+
 			child = (<Modal onSubmit={this.handleSubmit} />);
+		} else {
+			console.log('[App] render - this.state.mounted = false');
 		}
-		
+
 		return(
 			<div className="App">
-				<ReactCSSTransitionGroup 
+				<ReactCSSTransitionGroup
 					transitionName="example"
 					transitionEnterTimeout={500}
 					transitionLeaveTimeout={300}>
